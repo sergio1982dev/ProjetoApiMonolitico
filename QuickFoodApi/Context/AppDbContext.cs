176276f1529ac30c9688dd913ec5b3e04670299a
Sergio Aparecido <sergiopxp@gmail.com>
2023-10-19 17:07:58 -0300

    Criada solução API monolítica para comercio de lanches. Criada entidades Categoria/Lanches, configurada a ORM, classe de contexto, realizado migrations e população das tabelas

---
+++ QuickFoodApi/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickFoodApi.Models;
+
+namespace QuickFoodApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
